e7ad12a3c2f073292d5aaaeb135dd16aed0cfe90
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2025-03-23 21:02:09 -0400

    Remove unused imports from Sphinx configuration in docs/conf.py to streamline documentation generation.

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import sys
-import os
-import inspect
 import GPUQuantile
 
 project = 'GPUQuantile'
